54f8ff382fb3434b7598448d75abdd55421598f3
Atakan Serçe <atakan.serce1@gmail.com>
2025-12-05 18:10:24 +0000

    Initial commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -97,7 +97,7 @@ function LoginScreen() {
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5, delay: 0.2, ease: "easeInOut" }}
-                 className="mt-6"
+                 className="mt-4"
             >
                 <Button
                     size="lg"
